11a23e8bed077a17fbc2397ac423de2651d76351
hua <a4704704@gmail.com>
2023-11-26 16:40:31 +0800

    redesign index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+definePageMeta({
+  middleware: defineNuxtRouteMiddleware(() => {
+    console.log('[匿名中間件] 我是直接定義在頁面內的匿名中間件')
+  })
+})
+</script>
 
 <template>
-  <body>
-    <h1>Hello World</h1>
-  </body>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-gray-800">
+        這裡是首頁
+      </h1>
+      <div class="my-4 flex space-x-4">
+        <NuxtLink to="/modules">前往modules</NuxtLink>
+        <NuxtLink to="/composables">前往composables</NuxtLink>
+        <NuxtLink to="/plugins">前往plugins</NuxtLink>
+        <NuxtLink to="/middleware">前往middleware</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
